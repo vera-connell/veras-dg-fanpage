14ddca57b4173892a75bc285dc9ef7c2ce773811
Gerard Paapu <gerard@gerardpaapu.com>
2023-06-21 13:49:42 +1200

    fix: getting ready for dokku

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,15 @@ import dgRoutes from './routes/dgRoutes'
 const server = express()
 
 server.use(express.json())
-server.use(express.static(path.join(__dirname, '../public')))
+
 server.use('/api/v1/dg', dgRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static(path.join(__dirname, '../assets')))
+  server.use('/', express.static(path.join(__dirname, '../../public')))
+
+  server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html')) 
+  })
+}
 export default server
